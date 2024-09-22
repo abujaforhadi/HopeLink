1d0c2fbc8fc596918f81cfe70123d5dec928c7d4
abujaforhadi <abujaforhadi@gmail.com>
2024-09-23 00:00:40 +0600

    add

---
+++ assets/js/utility.js
@@ -0,0 +1,13 @@
+function findInputValue(id){
+    var id=document.getElementById(id).value;
+    id=parseFloat(id);
+    return id;
+
+}
+function findElementValue(id){
+    var id=document.getElementById(id).innerHTML;
+    id=parseFloat(id);
+    return id;
+
+}
+
